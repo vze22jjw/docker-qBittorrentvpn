4d84c2c69b5d89cf6b4b8216541407d9affbf533
Donald Webster <fryfrog@gmail.com>
2018-09-19 14:06:58 -0700

    Convert tabs to 2 spaces.

--- qbittorrent/start.sh
+++ qbittorrent/start.sh
@@ -1,43 +1,43 @@
 #!/bin/bash
 if [[ ! -e /config/qBittorrent ]]; then
-	mkdir -p /config/qBittorrent/config/
-	chown -R "${PUID}":"${PGID}" /config/qBittorrent
+  mkdir -p /config/qBittorrent/config/
+  chown -R "${PUID}":"${PGID}" /config/qBittorrent
 else
-	chown -R "${PUID}":"${PGID}" /config/qBittorrent
+  chown -R "${PUID}":"${PGID}" /config/qBittorrent
 fi
 
 if [[ ! -e /config/qBittorrent/config/qBittorrent.conf ]]; then
-	/bin/cp /etc/qbittorrent/qBittorrent.conf /config/qBittorrent/config/qBittorrent.conf
-	chmod 755 /config/qBittorrent/config/qBittorrent.conf
+  /bin/cp /etc/qbittorrent/qBittorrent.conf /config/qBittorrent/config/qBittorrent.conf
+  chmod 755 /config/qBittorrent/config/qBittorrent.conf
 fi
 
 # Set qBittorrent WebUI and Incoming ports
 if [ ! -z "${WEBUI_PORT}" ]; then
-	webui_port_exist=$(grep -m 1 "WebUI\Port=${WEBUI_PORT}" /config/qBittorrent/config/qBittorrent.conf)
-	if [[ -z "${webui_port_exist}" ]]; then
-		webui_exist=$(grep -m 1 'WebUI\Port' /config/qBittorrent/config/qBittorrent.conf)
-		if [[ ! -z "${webui_exist}" ]]; then
-			# Get line number of WebUI Port
-			LINE_NUM=$(grep -Fn -m 1 'WebUI\Port' /config/qBittorrent/config/qBittorrent.conf | cut -d: -f 1)
-			sed -i "${LINE_NUM}s@.*@WebUI\Port=${WEBUI_PORT}\n@" /config/qBittorrent/config/qBittorrent.conf
-		else
-			echo "WebUI\Port=${WEBUI_PORT}" >> /config/qBittorrent/config/qBittorrent.conf
-		fi
-	fi
+  webui_port_exist=$(grep -m 1 "WebUI\Port=${WEBUI_PORT}" /config/qBittorrent/config/qBittorrent.conf)
+  if [[ -z "${webui_port_exist}" ]]; then
+    webui_exist=$(grep -m 1 'WebUI\Port' /config/qBittorrent/config/qBittorrent.conf)
+    if [[ ! -z "${webui_exist}" ]]; then
+      # Get line number of WebUI Port
+      LINE_NUM=$(grep -Fn -m 1 'WebUI\Port' /config/qBittorrent/config/qBittorrent.conf | cut -d: -f 1)
+      sed -i "${LINE_NUM}s@.*@WebUI\Port=${WEBUI_PORT}\n@" /config/qBittorrent/config/qBittorrent.conf
+    else
+      echo "WebUI\Port=${WEBUI_PORT}" >> /config/qBittorrent/config/qBittorrent.conf
+    fi
+  fi
 fi
 
 if [ ! -z "${INCOMING_PORT}" ]; then
-	incoming_port_exist=$(grep -m 1 "Connection\PortRangeMin=${INCOMING_PORT}" /config/qBittorrent/config/qBittorrent.conf)
-	if [[ -z "${incoming_port_exist}" ]]; then
-		incoming_exist=$(grep -m 1 'Connection\PortRangeMin' /config/qBittorrent/config/qBittorrent.conf)
-		if [[ ! -z "${incoming_exist}" ]]; then
-			# Get line number of Incoming
-			LINE_NUM=$(grep -Fn -m 1 'Connection\PortRangeMin' /config/qBittorrent/config/qBittorrent.conf | cut -d: -f 1)
-			sed -i "${LINE_NUM}s@.*@Connection\PortRangeMin=${INCOMING_PORT}\n@" /config/qBittorrent/config/qBittorrent.conf
-		else
-			echo "Connection\PortRangeMin=${INCOMING_PORT}" >> /config/qBittorrent/config/qBittorrent.conf
-		fi
-	fi
+  incoming_port_exist=$(grep -m 1 "Connection\PortRangeMin=${INCOMING_PORT}" /config/qBittorrent/config/qBittorrent.conf)
+  if [[ -z "${incoming_port_exist}" ]]; then
+    incoming_exist=$(grep -m 1 'Connection\PortRangeMin' /config/qBittorrent/config/qBittorrent.conf)
+    if [[ ! -z "${incoming_exist}" ]]; then
+      # Get line number of Incoming
+      LINE_NUM=$(grep -Fn -m 1 'Connection\PortRangeMin' /config/qBittorrent/config/qBittorrent.conf | cut -d: -f 1)
+      sed -i "${LINE_NUM}s@.*@Connection\PortRangeMin=${INCOMING_PORT}\n@" /config/qBittorrent/config/qBittorrent.conf
+    else
+      echo "Connection\PortRangeMin=${INCOMING_PORT}" >> /config/qBittorrent/config/qBittorrent.conf
+    fi
+  fi
 fi
 
 echo "[info] Starting qBittorrent daemon..." | ts '%Y-%m-%d %H:%M:%.S'
@@ -49,10 +49,10 @@ qbpid=$(pgrep -o -x qbittorrent-nox)
 echo "[info] qBittorrent PID: $qbpid" | ts '%Y-%m-%d %H:%M:%.S'
 
 if [ -e "/proc/${qbpid}" ]; then
-	if [[ -e /config/qBittorrent/data/logs/qbittorrent.log ]]; then
-		chmod 775 /config/qBittorrent/data/logs/qbittorrent.log
-	fi
-	sleep infinity
+  if [[ -e /config/qBittorrent/data/logs/qbittorrent.log ]]; then
+    chmod 775 /config/qBittorrent/data/logs/qbittorrent.log
+  fi
+  sleep infinity
 else
-	echo "qBittorrent failed to start!"
+  echo "qBittorrent failed to start!"
 fi
