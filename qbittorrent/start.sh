0c93b9ed007bd595a64524889c130684918994c6
Donald Webster <fryfrog@gmail.com>
2018-09-19 16:00:05 -0700

    Clean up escaping in start.sh

--- qbittorrent/start.sh
+++ qbittorrent/start.sh
@@ -15,34 +15,32 @@ fi
 if [ -n "${WEBUI_PORT}" ]; then
   webui_port_exist=$(grep -m 1 "WebUI\\\Port=${WEBUI_PORT}" /config/qBittorrent/config/qBittorrent.conf)
   if [[ -z "${webui_port_exist}" ]]; then
-    webui_exist=$(grep -m 1 'WebUI\\\Port' /config/qBittorrent/config/qBittorrent.conf)
+    webui_exist=$(grep -m 1 'WebUI\Port' /config/qBittorrent/config/qBittorrent.conf)
     if [[ -n "${webui_exist}" ]]; then
       # Get line number of WebUI Port
-      LINE_NUM=$(grep -Fn -m 1 'WebUI\\\Port' /config/qBittorrent/config/qBittorrent.conf | cut -d: -f 1)
-      sed -i "${LINE_NUM}s@.*@WebUI\\\Port=${WEBUI_PORT}\n@" /config/qBittorrent/config/qBittorrent.conf
+      LINE_NUM=$(grep -Fn -m 1 'WebUI\Port' /config/qBittorrent/config/qBittorrent.conf | cut -d: -f 1)
+      sed -i "${LINE_NUM}s@.*@WebUI\Port=${WEBUI_PORT}\n@" /config/qBittorrent/config/qBittorrent.conf
     else
       echo "WebUI\Port=${WEBUI_PORT}" >> /config/qBittorrent/config/qBittorrent.conf
     fi
   fi
 fi
-
 echo "[info] qBittorrent WebUI port: ${WEBUI_PORT}" | ts '%Y-%m-%d %H:%M:%.S'
 
 # Set qBitTorrent incoming port
 if [ -n "${INCOMING_PORT}" ]; then
   incoming_port_exist=$(grep -m 1 "Connection\\\PortRangeMin=${INCOMING_PORT}" /config/qBittorrent/config/qBittorrent.conf)
   if [[ -z "${incoming_port_exist}" ]]; then
-    incoming_exist=$(grep -m 1 'Connection\\\PortRangeMin' /config/qBittorrent/config/qBittorrent.conf)
+    incoming_exist=$(grep -m 1 'Connection\PortRangeMin' /config/qBittorrent/config/qBittorrent.conf)
     if [[ -n "${incoming_exist}" ]]; then
       # Get line number of Incoming
-      LINE_NUM=$(grep -Fn -m 1 'Connection\\\PortRangeMin' /config/qBittorrent/config/qBittorrent.conf | cut -d: -f 1)
-      sed -i "${LINE_NUM}s@.*@Connection\\\PortRangeMin=${INCOMING_PORT}\n@" /config/qBittorrent/config/qBittorrent.conf
+      LINE_NUM=$(grep -Fn -m 1 'Connection\PortRangeMin' /config/qBittorrent/config/qBittorrent.conf | cut -d: -f 1)
+      sed -i "${LINE_NUM}s@.*@Connection\PortRangeMin=${INCOMING_PORT}\n@" /config/qBittorrent/config/qBittorrent.conf
     else
       echo "Connection\PortRangeMin=${INCOMING_PORT}" >> /config/qBittorrent/config/qBittorrent.conf
     fi
   fi
 fi
-
 echo "[info] qBittorrent incoming port: ${INCOMING_PORT}" | ts '%Y-%m-%d %H:%M:%.S'
 
 echo "[info] Starting qBittorrent daemon..." | ts '%Y-%m-%d %H:%M:%.S'
