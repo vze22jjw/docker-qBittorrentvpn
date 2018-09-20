a1a5a4daf69cb5a12ffb87638de345c5c8c17c45
Donald Webster <fryfrog@gmail.com>
2018-09-19 22:04:42 -0700

    Fixing port modification / adding.

--- root/start-qbittorrent.sh
+++ root/start-qbittorrent.sh
@@ -18,10 +18,10 @@ echo "[info] qBittorrent WebUI port: ${WEBUI_PORT}" | ts '%Y-%m-%d %H:%M:%.S'
 # Is the webui port already set correctly?
 if ! grep -q -m 1 "WebUI\\\Port=${WEBUI_PORT}" "${CONF_FILE}"; then
   # Is the webui port config option in the file?
-  if grep -q -m 1 'WebUI\Port' "${CONF_FILE}"; then
+  if grep -q -m 1 'WebUI\\\Port' "${CONF_FILE}"; then
     # Get line number of WebUI Port
     LINE_NUM=$(grep -Fn -m 1 'WebUI\Port' "${CONF_FILE}" | cut -d: -f 1)
-    sed -i "${LINE_NUM}s@.*@WebUI\Port=${WEBUI_PORT}\n@" "${CONF_FILE}"
+    sed -i "${LINE_NUM}s@.*@WebUI\\\Port=${WEBUI_PORT}\n@" "${CONF_FILE}"
     echo "[info] Modified existing WebUI Port in qBittorrent config." | ts '%Y-%m-%d %H:%M:%.S'
   else
     echo "WebUI\Port=${WEBUI_PORT}" >> "${CONF_FILE}"
@@ -35,10 +35,10 @@ echo "[info] qBittorrent incoming port: ${INCOMING_PORT}" | ts '%Y-%m-%d %H:%M:%
 # Is the incoming port set correctly?
 if ! grep -q -m 1 "Connection\\\PortRangeMin=${INCOMING_PORT}" "${CONF_FILE}"; then
   # Is incoming port config option in the file?
-  if grep -q -m 1 'Connection\PortRangeMin' "${CONF_FILE}"; then
+  if grep -q -m 1 'Connection\\\PortRangeMin' "${CONF_FILE}"; then
     # Get line number of Incoming
     LINE_NUM=$(grep -Fn -m 1 'Connection\PortRangeMin' "${CONF_FILE}" | cut -d: -f 1)
-    sed -i "${LINE_NUM}s@.*@Connection\PortRangeMin=${INCOMING_PORT}\n@" "${CONF_FILE}"
+    sed -i "${LINE_NUM}s@.*@Connection\\\PortRangeMin=${INCOMING_PORT}\n@" "${CONF_FILE}"
     echo "[info] Modified existing PortRangeMin in qBittorrent config." | ts '%Y-%m-%d %H:%M:%.S'
   else
     echo "Connection\PortRangeMin=${INCOMING_PORT}" >> "${CONF_FILE}"
